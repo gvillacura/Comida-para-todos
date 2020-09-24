a9d30e7aedbd217ca571d00e2bcd73f3a3869ba3
yndira <yndinatera10@gmail.com>
2020-09-24 20:13:15 -0300

    basic layout of the JoinUs template

--- src/components/joinus/JoinUs.js
+++ src/components/joinus/JoinUs.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import Header from '../header/Header';
 import Infogram from './Infrogram';
+import Quote from './Quote';
 import WaysTo from './WaysTo';
 
 const JoinUs = () => {
@@ -9,6 +10,7 @@ const JoinUs = () => {
             <Header />
             <Infogram />
             <WaysTo />
+            <Quote />
         </Fragment>
     )
 }
--- src/components/joinus/WaysTo.js
+++ src/components/joinus/WaysTo.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import './waysTo.css';
 
 const WaysTo = () => {
     return (
-        <div className="ways-container">
+        <div className="way">
             <h2>Formas de Colaborar</h2>
+            <div className="ways-container">
             <div className="ways">
                 <img src="" alt="way-to" />
                 <p>Lorem ipsum</p>
                 <button type="button">Colaborar</button>
+            </div>
+
+            <div className="ways">
+                <img src="" alt="way-to" />
+                <p>Lorem ipsum</p>
+                <button type="button">Colaborar</button>
+            </div>
+
+            <div className="ways">
+                <img src="" alt="way-to" />
+                <p>Lorem ipsum</p>
+                <button type="button">Colaborar</button>
+            </div>
             </div>
         </div>
     )
