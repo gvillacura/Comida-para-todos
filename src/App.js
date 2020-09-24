eae1fd1a433994281f49bcd2b5acca567f170374
yndira <yndinatera10@gmail.com>
2020-09-24 19:10:50 -0300

    router configuration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import JoinUs from './components/joinus/JoinUs';
+import AboutUs from './components/about/About';
+import Activities from './components/activities/Activities';
+import Home from './components/home/Home';
 
 function App() {
   return (
-    
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+
+        <Route path="/about">
+          <AboutUs />
+        </Route>
+
+        <Route path="/join">
+          <JoinUs />
+        </Route>
+
+        <Route path="/activities">
+          <Activities />
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/home/home.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import Header from '../header/Header';
+
+const Home = () => {
+    return(
+        <Fragment>
+            <Header/>
+            <h1>Home</h1>
+        </Fragment>
+    )
+}
+
+export default Home;
