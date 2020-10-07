41f3a5e64c6c5e174f69312a419481db94bbaf74
Geraldine Villacura R <geraldinevillacuraruz@gmail.com>
2020-10-07 11:00:44 -0300

    se agrega modal para llenar formulario de contacto en sección Sé parte más mensaje de éxito

--- src/components/modal/FormModal.js
+++ src/components/modal/FormModal.js
@@ -1,10 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./formModal.css";
 
 const FormModal = (props) => {
-  // const [disabledButton, setDisabledButton] = useState(true);
+  const [disabledButton, setDisabledButton] = useState(true);
 
-  // if(){setDisabledButton(false)}
+  const [data, setData] = useState({
+    nombre: "",
+    correo: "",
+    telefono: "",
+    donacion: "",
+  });
+
+  const handleInputChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    console.log(data);
+    if (
+      data.nombre !== "" &&
+      data.correo !== "" &&
+      data.telefono !== "" &&
+      data.donacion !== ""
+    ) {
+      console.log("OK");
+      setDisabledButton(false);
+    } else {
+      console.log("debe rellenar datos");
+      setDisabledButton(true);
+    }
+  }, [data]);
 
   return (
     <div className="modal-items">
@@ -12,16 +37,25 @@ const FormModal = (props) => {
       <h4 className="text-modal">
         Por favor, llena la solicitud y te contestaremos a la brevedad posible.
       </h4>
-      <p>Nombre y Apellido</p>
-      <input />
-      <p>Correo</p>
-      <input />
-      <p>Teléfono</p>
-      <input />
-      <p>Cuéntanos tu tipo de donación</p>
-      <input className="donation-type" />
+      <form>
+        <p>Nombre y Apellido</p>
+        <input type="text" name="nombre" onChange={handleInputChange} />
+        <p>Correo</p>
+        <input type="email" name="correo" onChange={handleInputChange} />
+        <p>Teléfono</p>
+        <input type="number" name="telefono" onChange={handleInputChange} />
+        <p>Cuéntanos tu tipo de donación</p>
+        <input
+          className="donation-type"
+          type="text"
+          name="donacion"
+          onChange={handleInputChange}
+        />
+      </form>
       <div className="send-btn">
-        <button onClick={props.open}>ENVIAR</button>
+        <button type="submit" onClick={props.open} disabled={disabledButton}>
+          ENVIAR
+        </button>
       </div>
     </div>
   );
