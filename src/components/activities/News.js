9bf929454619b0407cfbb20a9bc5f88b40486e5d
Geraldine Villacura R <geraldinevillacuraruz@gmail.com>
2020-10-07 17:20:22 -0300

    try to add instagram embed

--- src/components/activities/News.js
+++ src/components/activities/News.js
@@ -1,39 +1,69 @@
-import React from 'react';
-import './news.css'
+import React, from "react";
+import InstagramEmbed from "react-instagram-embed";
+import "./news.css";
 
 const News = () => {
-    return (
-        <div className="newsContainer">
-            <div class='embedsocial-instagram' data-ref="8425c9ad5e46de1107ee0b230f69b9c3b7cc022d"></div>
-            <h2 className="newsContainerTitle"> Noticias </h2>
-            <div className="newsContent">
-                <div className="new">
-                    <img src="" alt="newImg" />
-                    <h4 className="newsTitle">"Lorem ipsum "</h4>
-                    <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                    </p>
-                </div>
-                <div className="new">
-                    <img src="" alt="newImg" />
-                    <h4 className="newsTitle">"Lorem ipsum "</h4>
-                    <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                    </p>
-                </div>
-                <div className="new">
-                    <img src="" alt="newImg" />
-                    <h4 className="newsTitle">"Lorem ipsum "</h4>
-                    <p> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                    </p>
-                </div>
-            </div>
+ 
+ 
+  return (
+    <div className="newsContainer">
+      <div className="int-cont">
+        <InstagramEmbed
+          url="https://www.instagram.com/p/CF0a27LFkjc/"
+          maxWidth={320}
+          hideCaption={true}
+          containerTagName="div"
+          protocol=""
+          injectScript={false}
+          onLoading={() => {}}
+          onSuccess={() => {}}
+          onAfterRender={() => {}}
+          onFailure={() => {}}
+        />
+        <InstagramEmbed
+          url="https://www.instagram.com/p/CF0a27LFkjc/"
+          maxWidth={320}
+          hideCaption={true}
+        />
+      </div>
+      <h2 className="newsContainerTitle"> Noticias </h2>
+      <div className="newsContent">
+        <div className="new">
+          <img src="" alt="newImg" />
+          <h4 className="newsTitle">"Lorem ipsum "</h4>
+          <p>
+            {" "}
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat."
+          </p>
         </div>
-    )
-}
+        <div className="new">
+          <img src="" alt="newImg" />
+          <h4 className="newsTitle">"Lorem ipsum "</h4>
+          <p>
+            {" "}
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat."
+          </p>
+        </div>
+        <div className="new">
+          <img src="" alt="newImg" />
+          <h4 className="newsTitle">"Lorem ipsum "</h4>
+          <p>
+            {" "}
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat."
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default News;
