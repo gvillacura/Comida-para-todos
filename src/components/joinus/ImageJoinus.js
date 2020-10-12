f9807e0931998a0d95777afa6727f6a2d8a949cc
yndira <yndinatera10@gmail.com>
2020-10-12 19:17:47 -0300

    responsive hero is added

--- src/components/joinus/ImageJoinus.js
+++ src/components/joinus/ImageJoinus.js
@@ -1,10 +1,14 @@
 import React from "react";
 import heroText from "../../img/frase-hero.png";
+import heroTextA from "../../img/frase-heroA.png";
 
 const ImageJoinus = () => {
   return (
     <div>
-      <img src={heroText} alt="hero text img" />
+      <picture>
+        <source srcSet={heroTextA} media="(max-width:376px)" />
+        <img src={heroText} alt="hero text img" />
+      </picture>
     </div>
   );
 };
