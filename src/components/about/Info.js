9deca6ca164f1116500e4931e16cde4bc70843fd
yndira <yndinatera10@gmail.com>
2020-10-13 13:55:56 -0300

    showMore component and funcionality added

--- src/components/about/Info.js
+++ src/components/about/Info.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./info.css";
 import usHero from "../../img/usHero.png";
 import usHeroA from "../../img/usHeroA.png";
+import ShowMore from "./showMore";
+import chevronDown from '@iconify/icons-feather/chevron-down';
+import chevronUp from '@iconify/icons-feather/chevron-up';
+
+
 
 const Info = () => {
+
+  const [textShow, setTextShow] = useState('Ver más');
+  const [icon, setIcon] = useState(chevronDown);
+  const p1 = useRef(null);
+  const p2 = useRef(null);
+  const p3 = useRef(null);
+  const btnShow = useRef(null);
+
+  const showMore = () => {
+    if (textShow === 'Ver más') {
+      setTextShow('Ver menos');
+      setIcon(chevronUp);
+      p1.current.style.display = 'block';
+      p2.current.style.display = 'block';
+      p3.current.style.display = 'block';
+    } else {
+      setTextShow('Ver más');
+      setIcon(chevronDown);
+      p1.current.style.display = 'none';
+      p2.current.style.display = 'none';
+      p3.current.style.display = 'none';
+    }
+  }
+
   return (
     <div className="container-info">
       <picture>
@@ -27,7 +56,7 @@ const Info = () => {
         gastronomía social es:{" "}
         <strong>alimentar el espíritu y cuerpo de quienes lo necesiten</strong>.
       </p>
-      <p>
+      <p ref={p1}>
         De esta manera fuimos capaces de unirnos treinta y cuatro organizaciones
         en Chile y España, para actuar y ponernos al servicio de quienes lo
         necesiten,{" "}
@@ -36,19 +65,22 @@ const Info = () => {
         de empleo, creando nuestra primera escuela con huerto urbano y lo más
         importante <strong>alimentando a miles de personas</strong>.{" "}
       </p>
-      <p>
+      <p ref={p2}>
         Trabajamos para a <strong>combatir el hambre en Chile</strong>,
         promoviendo una alimentación de calidad y nutritiva; conectando,
         colaborando y generando asociatividad entre los actores de la cadena
         gastronómica y la ciudadanía.{" "}
       </p>
-      <p>
+      <p ref={p3}>
         Un plato bueno que genera empleo, fomenta la educación y fortalece las
         comunidades. Un camino que necesita de la ciudadanía y el empresariado
         para poder seguir juntos llevando a Chile y cualquier otro país a lograr
         Seguridad Alimentaria y los objetivos de desarrollo sostenible (ODS) que
         de ésta se desprenden.{" "}
       </p>
+      { window.matchMedia("(max-width: 400px)").matches === true ? 
+        <ShowMore show={showMore} text={textShow} refs={btnShow} icon={icon}/> 
+        : console.log('no match') }
     </div>
   );
 };
